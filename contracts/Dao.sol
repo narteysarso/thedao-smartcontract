881a615a825ac23605d8bbad6426295f7dcafcb5
narteysarso <narteysarso@gmail.com>
2022-02-12 22:05:22 +0000

    .

--- contracts/Dao.sol
+++ contracts/Dao.sol
@@ -7,10 +7,12 @@ contract Dao{
     address owner;
 
     //registration fee
-    uint256 registrationFee = 1 wei;
+    uint256 registrationFee = 100000000000000 wei;
 
     struct Member{
         uint256 balance;
+        string username;
+        string imageUrl;
         mapping(string => bool) votes;
     }
 
@@ -20,8 +22,9 @@ contract Dao{
     //proposal 
     struct Proposal{
         address author;
-        bool isActive;
+        bool isClosed;
         uint256 totalVotes;
+        uint256 endDate;
         mapping(string => bool) options;
     }
 
@@ -29,18 +32,26 @@ contract Dao{
     mapping(string => Proposal) public proposals;
 
     //create proposal event
-    event proposalCreated(
-        address indexed sender,
-        string indexed proposal
+    event ProposalCreated(
+        address indexed _author,
+        string indexed _proposal,
+        string proposal,
+        string author,
+        string[] options,
+        uint256 timestamp
     );
 
     //cast vote event
-    event castVote(
+    event VoteCasted(
+        address indexed _sender,
+        string indexed _voteOption,
+        string indexed _proposal,
         uint256 lastVoteCount,
         uint256 newVoteCount,
-        address sender,
-        string indexed voteOption,
-        string indexed proposal
+        string author,
+        string imageUrl,
+        string voteOption,
+        uint256 timestamp
     );
 
     constructor(){
@@ -48,69 +59,73 @@ contract Dao{
     }
 
     //register members;
-    function registerMember() public payable {
-        require(msg.value == registrationFee, "Registration fee is 1 wei");
+    function registerMember(string memory username, string memory imageUrl) public payable {
+        require(msg.value == registrationFee, "Registration fee is 0.0001 ether");
         //get a pointer for new member "object"
         Member storage m =  members[msg.sender];
         m.balance = msg.value;
+        m.username = username;
+        m.imageUrl = imageUrl;
     }
 
     //create proposal
-    function createProposal(string memory _title, bool _isActive) external {
+    function createProposal(string memory _title, string[] memory voteOptions, uint256 endDate) external {
         require(members[msg.sender].balance == registrationFee, "You are not authorized to take this action");
         //get a pointer for a new proposal "object"
         Proposal storage p = proposals[_title];
         p.author = msg.sender;
-        p.isActive = _isActive;
 
-        emit proposalCreated(msg.sender, _title);
+        if(endDate > 0){
+            p.endDate = endDate;
+        }
+
+        for(uint len = 0; len < voteOptions.length; len++){
+            p.options[voteOptions[len]] = true;
+        }
+        
+        emit ProposalCreated(msg.sender , _title, _title, members[msg.sender].username,   voteOptions, block.timestamp);
     }
 
-    function addProposalVoteOption(string memory _proposal, string memory option) external {
-         require(members[msg.sender].balance == registrationFee, "You are not authorized to take this action");
-        require(proposals[_proposal].author == msg.sender, "You are not authorized to take this action");
-
-        proposals[_proposal].options[option] = true;
-    }
-
-    //mark proposal inactive
-    function markProposalInActive(string memory _title) external {
+    //mark proposal open
+    function markProposalOpen(string memory _title) external {
         require(members[msg.sender].balance == registrationFee, "You are not authorized to take this action");
         require(proposals[_title].author == msg.sender, "You are not authorized to take this action");
 
-        proposals[_title].isActive = false;
+        proposals[_title].isClosed = false;
     }
 
-    //mark proposal active
-    function markProposalActive(string memory _title) external {
+    //mark proposal closed
+    function markProposalClosed(string memory _title) external {
         require(members[msg.sender].balance == registrationFee, "You are not authorized to take this action");
         require(proposals[_title].author == msg.sender, "You are not authorized to take this action");
 
-        proposals[_title].isActive = true;
+        proposals[_title].isClosed = true;
     }
 
     //cast votes;
     function vote(string memory _proposal, string memory _voteOption ) external {
         require(members[msg.sender].balance == registrationFee, "You are not authorized to take this action");
-        require(proposals[_proposal].isActive, "Proposal not available for voting" );
+        require(!proposals[_proposal].isClosed, "Proposal not available for voting" );
         require(proposals[_proposal].options[_voteOption], "Vote option is not available" );
-        require(!members[msg.sender].votes[_proposal], "You votes have been cast already");
+        require(members[msg.sender].votes[_proposal] == false , "You vote has been cast already");
 
         uint256 votes = proposals[_proposal].totalVotes;
         members[msg.sender].votes[_proposal] = true;
         proposals[_proposal].totalVotes = votes + 1;
 
         //log vote cast
-        emit castVote(votes, votes+1, msg.sender, _voteOption, _proposal);
+        emit VoteCasted(msg.sender , _voteOption, _proposal, votes, votes+1, members[msg.sender].username, members[msg.sender].imageUrl,_voteOption, block.timestamp);
     }
 
-    //get votes results;
-    function getTotalVotes(string memory _proposal) external view returns (uint256) {
-        require(proposals[_proposal].isActive, "Proposal not found" );
-        return proposals[_proposal].totalVotes;
+    //get proposal attributes;
+    function getProposalAttributes(string memory _proposal) external view returns (uint256, bool) {
+        return (proposals[_proposal].totalVotes, proposals[_proposal].isClosed);
     }
 
-    //log proposal events
+    //check registration
+    function checkRegistered(address _address) external view returns (bool){
+        return members[_address].balance == registrationFee ;
+    }
 
 
     //get organization balance
--- hardhat.config.js
+++ hardhat.config.js
@@ -7,6 +7,10 @@ module.exports = {
     rinkeby: {
       url: `${process.env.ALCHEMY_RINKEBY_URL}`,
       accounts: [`${process.env.RINKEBY_PRIVATE_KEY}`],
-    } 
+    },
+    ganache:{
+      url: 'http://localhost:7545',
+      accounts: ["044d59887f2394de7054e7436e0081f5a6c6fbbf2c88beb5e28334176f0b45c2"]
+    }
   }
 };
